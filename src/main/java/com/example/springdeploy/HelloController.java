9c7e521eaa189630d25bbe6eb1a2fdd7cd9bd099
jorgegomez <jorgegomez.dev@gmail.com>
2022-09-23 18:49:15 -0300

    spring heroku app config

---
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -0,0 +1,39 @@
+package com.example.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        return "Hola Mundo como estas crack!!!";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hola Mundo desde SpringBoot!</h1>
+                    <a class="btn btn-primary" href="https://www.google.com"> Google </a>                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                             
+                """;
+    }
+}
